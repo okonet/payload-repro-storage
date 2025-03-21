b125a95560b45cbdb443093d4bbf5693dfe31fe7
Andrey Okonetchnikov <andrey@okonet.dev>
2025-03-21 11:30:33 +0100

    Add vercel storage

--- apps/admin/src/payload.config.ts
+++ apps/admin/src/payload.config.ts
@@ -2,6 +2,7 @@
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import {vercelBlobStorage} from "@payloadcms/storage-vercel-blob";
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
@@ -33,5 +34,11 @@ export default buildConfig({
   plugins: [
     payloadCloudPlugin(),
     // storage-adapter-placeholder
+    vercelBlobStorage({
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || "",
+    }),
   ],
 })
